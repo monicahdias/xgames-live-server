9f2766d6f4273656ef909e4d83f7e5fffa252a65
Monica Dias <msumiedias@gmail.com>
2022-06-02 01:41:31 -0300

    feat(user): create entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+export class User {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  cpf: string;
+  isAdmin: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}

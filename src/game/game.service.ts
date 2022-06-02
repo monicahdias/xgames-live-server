88fca20d5e8314927efce14bbdd5fc7fa56fe5e7
Monica Dias <msumiedias@gmail.com>
2022-06-01 23:43:36 -0300

    chore(game): implement not found error for 'update' & 'delete'

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
@@ -28,20 +32,30 @@ export class GameService {
   create(dto: CreateGameDto): Promise<Game> {
     const data: Game = { ...dto };
 
-    return this.prisma.game.create({ data });
+    return this.prisma.game.create({ data }).catch(this.handleError);
   }
 
   async update(id: string, dto: UpdateGameDto): Promise<Game> {
     await this.findById(id);
     const data: Partial<Game> = { ...dto };
 
-    return this.prisma.game.update({
-      where: { id },
-      data,
-    });
+    return this.prisma.game
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
   async delete(id: string) {
     await this.findById(id);
     await this.prisma.game.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'An error occurred while processing your request',
+    );
   }
 }

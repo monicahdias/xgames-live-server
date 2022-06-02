f67935059c73c270ef2722a97ae8cb1b3d592aaf
Monica Dias <msumiedias@gmail.com>
2022-06-02 01:42:27 -0300

    feat(user): implement Service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  findAll() {
-    return `This action returns all user`;
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async findById(id: string): Promise<User> {
+    const record = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Record with ID '${id}' not found`);
+    }
+    return record;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<User> {
+    return this.findById(id);
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  create(dto: CreateUserDto): Promise<User> {
+    const data: User = { ...dto };
+
+    return this.prisma.user.create({ data }).catch(this.handleError);
   }
 
-  delete(id: string) {
-    return `This action removes a #${id} user`;
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
+    await this.findById(id);
+    const data: Partial<User> = { ...dto };
+
+    return this.prisma.user
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
+  }
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.user.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+    if (!lastErrorLine) {
+      console.error(error);
+    }
+
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
+    );
   }
 }

aa62ce39123f6763b6afe8b93f5836d9545c2df6
Monica Dias <msumiedias@gmail.com>
2022-06-01 22:09:44 -0300

    docs: implement swagger

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,9 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsString, IsUrl } from 'class-validator';
+
 export class CreateGameDto {
+  @IsString()
+  @ApiProperty({
+    description: 'The name of the game',
+    example: 'Valheim',
+  })
   title: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'The URL of cover image of the game',
+    example:
+      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+  })
   coverImageUrl: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'The description of the game',
+    example: 'A game about the great Valheim',
+  })
   description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'The year of the game',
+    example: 2020,
+  })
   year: number;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'The IMDB score of the game',
+    example: 5,
+  })
   imdbScore: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'The URL of the game trailer',
+    example: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  })
   trailerYouTubeUrl: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'The URL of the game gameplay',
+    example: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  })
   gameplayYouTubeUrl: string;
 }
--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateGameDto } from './dto/create-game.dto';
 import { GameService } from './game.service';
 
+@ApiTags('game')
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}

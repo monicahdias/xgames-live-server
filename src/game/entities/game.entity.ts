dd3d60773523907701c576b7777729030efcce5c
Monica Dias <msumiedias@gmail.com>
2022-06-01 21:55:26 -0300

    feat(game): implement create game endpoint

---
+++ src/game/entities/game.entity.ts
@@ -0,0 +1,10 @@
+export class Game {
+  id: string;
+  title: string;
+  coverImageUrl: string;
+  description: string;
+  year: number;
+  imdbScore: number;
+  trailerYouTubeUrl: string;
+  gameplayYouTubeUrl: string;
+}
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
+import { Game } from './entities/game.entity';
 
 @Injectable()
 export class GameService {
+  games: Game[] = [];
+
   findAll() {
     return 'Find all games';
   }
 
   create(createGameDto: CreateGameDto) {
-    return 'Create a new game' + JSON.stringify(createGameDto);
+    const game: Game = { id: 'random_id', ...createGameDto };
+
+    this.games.push(game);
+
+    return game;
   }
 }

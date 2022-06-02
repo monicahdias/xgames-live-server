1030d36ab368abf85b516e035bd2f8f054059a36
Monica Dias <msumiedias@gmail.com>
2022-06-02 01:52:36 -0300

    fix(user): remove 'confirmPassword' when creating/updating

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -30,6 +30,8 @@ export class UserService {
   }
 
   create(dto: CreateUserDto): Promise<User> {
+    delete dto.confirmPassword;
+
     const data: User = { ...dto };
 
     return this.prisma.user.create({ data }).catch(this.handleError);
@@ -37,6 +39,9 @@ export class UserService {
 
   async update(id: string, dto: UpdateUserDto): Promise<User> {
     await this.findById(id);
+
+    delete dto.confirmPassword;
+
     const data: Partial<User> = { ...dto };
 
     return this.prisma.user
@@ -54,12 +59,13 @@ export class UserService {
   handleError(error: Error): undefined {
     const errorLines = error.message?.split('\n');
     const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
     if (!lastErrorLine) {
       console.error(error);
     }
 
     throw new UnprocessableEntityException(
-      lastErrorLine || 'Algum erro ocorreu ao executar a operação',
+      lastErrorLine || 'An error occurred while processing your request',
     );
   }
 }
